26791029b94bf46f104fd1975ba5ef9719aff52c
Mioriarty <moritz.seppelt@googlemail.com>
2021-11-05 13:11:41 +0100

    US 2 renaming solve veginere

---
+++ 02_vigenere.py
@@ -0,0 +1,72 @@
+import include.utils as utils
+additiv = __import__("01_additiv")
+
+def encipher(text: str, key: str) -> str:
+    return "".join([ chr((ord(c) + ord(key[i % len(key)])) % 128) for i, c in enumerate(text) ])
+
+def decipher(text: str, key: str) -> str:
+    return "".join([ chr((ord(c) - ord(key[i % len(key)]) + 128) % 128) for i, c in enumerate(text) ])
+
+def determineBestKeyLength(text: str, minLength : int = 2, maxLength : int = 60) -> int:
+    icValues = {}
+    for keyLength in range(minLength, maxLength + 1):
+        icTotal = 0
+        for groupIndex in range(keyLength):
+            charCounts = {}
+            charNum = 0
+            for i in range(groupIndex, len(text), keyLength):
+                charNum += 1
+                if text[i] in charCounts:
+                    charCounts[text[i]] += 1
+                else:
+                    charCounts[text[i]] = 1
+
+            icTotal += 1/(charNum * (charNum-1)) * sum([count * (count-1) for count in charCounts.values()])
+
+        icValues[keyLength] = icTotal / keyLength
+
+    bestKeyLength = max(icValues, key=icValues.get)
+
+    # look for a smaller one that fits just as good
+    EPSILON = 1e-3
+    for i in range(minLength, bestKeyLength):
+        if icValues[i] >= icValues[bestKeyLength] - EPSILON:
+            return i
+    
+    # no better key length found
+    return bestKeyLength
+
+def determineKeyFromKeyLength(text : str, keyLength : int) -> str:
+    return "".join([ chr(additiv.determineBestKey(text[groupIndex::keyLength])) for groupIndex in range(keyLength) ])
+
+
+if(__name__ == '__main__'):
+    print("***** Vigenère Cipher *****")
+    if(utils.yesNoQuestion("Du you want to encipher?")):
+        text = utils.textFromFileOrConsole()
+        key = input("Insert the key: ")
+
+        print("Enciphered text:")
+        print(encipher(text, key))
+
+
+    else:
+        text = utils.textFromFileOrConsole()
+        
+        if(utils.yesNoQuestion("Do you want to determine the key automatically?")):
+            keyLength = determineBestKeyLength(text)
+            print(f"Best key length {keyLength}")
+
+            key = determineKeyFromKeyLength(text, keyLength)
+            print(f"Best key: {key}")
+
+            print("Deciphered text:")
+            print(decipher(text, key))
+        
+        else:
+            key = input("Insert the key: ")
+
+            print(f"Deciphered text:")
+            print(decipher(text, key))
+
+
